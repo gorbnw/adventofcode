836dcfeff550bd7958d66b5971a0d2dd4981b173
Gordon B <gordon.indeed@gmail.com>
2017-12-21 19:13:58 -0800

    Solve day 6

---
+++ 6/day6pt2.rb
@@ -0,0 +1,40 @@
+require 'set'
+
+def process_input(file_name)
+  File.read(file_name).chomp.split("\t").map(&:to_i)
+end
+
+def reallocate(array, start, quantity)
+  array[start] = 0
+  start += 1
+  while quantity > 0
+    array[start % array.length] += 1
+    start += 1
+    quantity -= 1
+  end
+  return array
+end
+
+
+def solve(file_name)
+  input = process_input(file_name)
+  look_up = Set.new
+  look_up.add(input)
+  steps = 0
+  flag = false
+  loop do
+    steps += 1 if flag
+    max = input.max
+    start = input.find_index(max)
+    reallocated = reallocate(input, start, max)
+    if look_up.include?(reallocated) && !flag
+      flag = true
+      look_up.clear
+    elsif look_up.include?(reallocated) && flag
+      return steps
+    end
+    look_up.add(reallocated)
+  end
+end
+
+p solve("6.txt")
